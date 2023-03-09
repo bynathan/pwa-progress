b50dedb5eb6484bc1e01ea37dff4bd8092b75afd
Nathan da Silva Lira <108292134+bynathan@users.noreply.github.com>
2023-03-08 21:03:48 -0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  hasIam: true,
+  apiUrl: 'https://j74ra94u5k.execute-api.us-east-2.amazonaws.com',
+  apmServer: 'http://localhost:8200',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  hasIam: true,
+  apiUrl: 'https://j74ra94u5k.execute-api.us-east-2.amazonaws.com',
+  // apiUrl: 'http://localhost',
+  apmServer: 'http://localhost',
+};
